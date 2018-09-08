b4979e56de79b74019026ae06bed06c51d7c4407
Ravi Simon <simonravi@yahoo.com>
2018-09-08 13:44:18 +0530

    Added slash to urls

--- apps/system/api_urls.py
+++ apps/system/api_urls.py
@@ -17,29 +17,29 @@ from apps.system.api_views import (
 
 urlpatterns = [
     path('', ProjectList.as_view()),
-    path('<int:pk>', ProjectDetail.as_view()),
-    path('<int:project_id>/apps', ProjectAppList.as_view()),
-    path('<int:project_id>/apps/<int:pk>', ProjectAppDetail.as_view()),
+    path('<int:pk>/', ProjectDetail.as_view()),
+    path('<int:project_id>/apps/', ProjectAppList.as_view()),
+    path('<int:project_id>/apps/<int:pk>/', ProjectAppDetail.as_view()),
 
-    path('<int:project_id>/systems', SystemList.as_view()),
-    path('<int:project_id>/systems/<int:pk>', SystemDetail.as_view()),
+    path('<int:project_id>/systems/', SystemList.as_view()),
+    path('<int:project_id>/systems/<int:pk>/', SystemDetail.as_view()),
 
     path(
-        '<int:project_id>/systems/<int:pk>/dependencies',
+        '<int:project_id>/systems/<int:pk>/dependencies/',
         SystemDependencyList.as_view()),
     path(
-        '<int:project_id>/systems/<int:system_id>/dependencies/<int:pk>',
+        '<int:project_id>/systems/<int:system_id>/dependencies/<int:pk>/',
         SystemDependencyDetail.as_view()),
     path(
-        '<int:project_id>/systems/<int:system_id>/moderators',
+        '<int:project_id>/systems/<int:system_id>/moderators/',
         SystemModeratorList.as_view()),
     path(
-        '<int:project_id>/systems/<int:system_id>/moderators/<int:pk>',
+        '<int:project_id>/systems/<int:system_id>/moderators/<int:pk>/',
         SystemModeratorDetail.as_view()),
     path(
-        '<int:project_id>/systems/<int:system_id>/apps',
+        '<int:project_id>/systems/<int:system_id>/apps/',
         SystemAppList.as_view()),
     path(
-        '<int:project_id>/systems/<int:system_id>/apps/<int:pk>',
+        '<int:project_id>/systems/<int:system_id>/apps/<int:pk>/',
         SystemAppDetail.as_view()),
 ]
