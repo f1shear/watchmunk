e4966dcff105f072d93274b80f2861184fe11b9f
Ravi Simon <simonravi@yahoo.com>
2018-09-11 00:43:12 +0530

    Removed get_context_data; as content is loaded by Ajax

--- apps/website/views.py
+++ apps/website/views.py
@@ -12,30 +12,3 @@ class LandingPage(TemplateView):
 
 class DashboardPage(TemplateView):
     template_name = 'website/dashboard.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['projects'] = ProjectModel.objects.all()
-        return context
-
-
-class ProjectPage(TemplateView):
-    template_name = 'website/project.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['project'] = get_object_or_404(
-            ProjectModel, id=self.kwargs.get('project_id'))
-        context['systems'] = SystemModel.objects.filter(
-            project_id=self.kwargs.get('project_id'))
-        return context
-
-
-class SystemPage(TemplateView):
-    template_name = 'website/system.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['system'] = get_object_or_404(
-            SystemModel, id=self.kwargs.get('system_id'))
-        return context
